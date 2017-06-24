74efb4856034ab53d1b710df297559ddb078fe7f
cstefanache <cornel.stefanache@gmail.com>
2017-06-24 21:49:32 +0300

    refactor(electron-backend): update storage file with remote sync

--- app/backend/Storage.js
+++ app/backend/Storage.js
@@ -1,18 +1,59 @@
 const PouchDB = require('pouchdb');
+/**
+ * CouchDB
+ */
+const couch = {
+    auth: {
+        username: 'coreid',
+        password: 'coreidforevar'
+    }
+}
+
+console.log(couch);
+let cache;
 
 class Storage {
 
+    static open(id) {
+        if (!cache) {
+            if (!id) {
+                throw new Error('Storage was not initialized with any id');
+            }
+            cache = new Storage(id);
+        }
+
+        return Promise.resolve(cache);
+    }
+
     constructor(id) {
         this.store = new PouchDB(`documents-${id}`);
-        this.store.info().then(data => {
-            console.log(data);
-        }).catch(err => {
+        this.localFeed = new PouchDB(`users-${id}`);
+        this.remoteFeed = new PouchDB(`http://138.197.8.148:5984/users-${id}`, couch);
+
+        this.localFeed.sync(this.remoteFeed, {
+            live: true
+        }).on('change', function (change) {
+            console.log(change)
+        }).on('error', function (err) {
             console.log(err);
+        });
+        this.localFeed.allDocs({
+            include_docs: true,
+            attachments: true
+        }).then(data => {
+            console.log(data.rows);
         })
+
+
     }
 
     put(doc) {
         return this.store.put(doc);
+    }
+
+    putToFeed(doc) {
+        delete doc._rev;
+        return this.localFeed.put(doc);
     }
 
     get(id) {
@@ -27,7 +68,7 @@ class Storage {
     }
 
     remove(doc) {
-        return this.store.remove(doc);
+        return this.store.remove(doc).catch(err => console.log);
     }
 
 }
--- app/backend/index.js
+++ app/backend/index.js
@@ -2,22 +2,4 @@ const Storage = require('./Storage');
 const Wallet = require('./Wallet');
 
 
-/**
- * This is for the demo purpose - subject to change
- */
-let _documentStore;
-
-const initializeStore = (id) => {
-    _documentStore = new Storage(id)
-    return _documentStore;
-};
-
-const documentStore = () => {
-    return _documentStore;
-};
-
-
-
-
-
-module.exports = {Storage, initializeStore, documentStore, Wallet};
+module.exports = {Storage, Wallet};
