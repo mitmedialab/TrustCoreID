51e44186123aeb74f565a92a7642f3c751c087f6
Keeley Erhardt <keeley.erhardt@gmail.com>
2017-06-26 16:44:04 -0300

    Switch from local ethereum node to TestRPC web3 provider

--- app/backend/EthereumClient.js
+++ app/backend/EthereumClient.js
@@ -7,7 +7,7 @@ const CryptoJS = require('crypto-js')
 const fs = require('fs')
 const keyto = require('@trust/keyto')
 const solc = require('solc')
-//const TestRPC = require('ethereumjs-testrpc')
+const TestRPC = require('ethereumjs-testrpc')
 const Tx = require('ethereumjs-tx')
 const Web3 = require('web3')
 
@@ -21,13 +21,12 @@ class EthereumClient {
 
   constructor(privateKey) {
     this.secretKey = keyto.from(privateKey.handle, 'pem').toString('blk', 'private')
-    //this.web3 = new Web3(TestRPC.provider({
-      //accounts: [{
-          //secretKey: '0x' + secretKey,
-          //balance: '0x100000000000000000'  // fund newly generated account
-        //}]
-    //}))
-    this.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+    this.web3 = new Web3(TestRPC.provider({
+      accounts: [{
+          secretKey: '0x' + this.secretKey,
+          balance: '0x100000000000000000'  // fund newly generated account
+        }]
+    }))
   }
 
   /**
@@ -36,37 +35,25 @@ class EthereumClient {
   putItOnTheBlockchain(dataHash) {
     let address = this.computeEthereumAddressFromPrivateKey()
     let compiledContract = this.compile(SMART_CONTRACT_FILE)
-    let tx = this.createRawTransaction(compiledContract.bytecode, address, '0x0')
     let Key = new Buffer(this.secretKey, 'hex')
-    tx.sign(Key)
-    let serializedTx = tx.serialize()
 
-    return new Promise((resolve, reject) => {
-        return this.web3.eth.sendRawTransaction(serializedTx.toString('hex'), (err, txHash) => {
-          if (err) {
-            console.error(err)
-            reject(err)
-          } else {
-            resolve(txHash)
-          }
-        })
+    return Promise.resolve() 
+    
+      .then(() => {
+        return this.createRawTransaction(compiledContract.bytecode, address, '0x0')
+      })
+
+      .then(tx => {
+        tx.sign(Key)
+        return tx.serialize()
+      })
+
+      .then(serializedTx => {
+        return this.sendRawTransaction(serializedTx.toString('hex'))
       })
 
       .then(txHash => {
-        let blockFilter = this.web3.eth.filter('latest');
-        return new Promise((resolve, reject) => {
-          blockFilter.watch( () => {
-            this.web3.eth.getTransactionReceipt(txHash, (err, receipt) => {
-              if (err) {
-                reject(err)
-              }
-              if (receipt) {
-                blockFilter.stopWatching()
-                resolve(receipt)
-              }
-            })
-          })
-        })
+        return this.getAsyncTransactionReceipt(txHash)
       })
 
       .catch(console.log)
@@ -77,40 +64,99 @@ class EthereumClient {
    */
   getAsyncTransactionReceipt(txHash) {
     let blockFilter = this.web3.eth.filter('latest')
-    let promise = new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       blockFilter.watch( () => {
         this.web3.eth.getTransactionReceipt(txHash, (err, receipt) => {
           if (err) {
-            console.error(err)
             reject(err)
-          }
-          if (receipt) {
-            blockFilter.stopWatching()
-            resolve(receipt)
+          } else {
+            blockFilter.stopWatching(() => {
+              resolve(receipt)
+            })
           }
         })
       })
     })
-    return promise
   }
 
   /**
    * createRawTransaction
    */
   createRawTransaction(bytecode, from) {
-    let _nonce = this.web3.toHex(this.web3.eth.getTransactionCount(from))
-    let _gasPrice = this.web3.toHex(this.web3.eth.gasPrice)
-    let _gasLimit = this.web3.toHex(3000000)
+    let gasLimit = this.web3.toHex(3000000)
+    let gasPrice
 
-    let _rawTx = {
-      nonce: _nonce,
-      gasPrice: _gasPrice,
-      gasLimit: _gasLimit,
-      data: '0x' + bytecode,
-      from: from
-    }
+    return Promise.resolve() 
 
-    return new Tx(_rawTx)
+      .then(() => {
+        return this.getGasPrice()
+      })
+
+      .then(price => {
+        gasPrice = price
+        return this.getTransactionCount(from)
+      })
+
+      .then(txCount => {
+        let _rawTx = {
+          nonce: this.web3.toHex(txCount),
+          gasPrice: this.web3.toHex(gasPrice),
+          gasLimit: gasLimit,
+          data: '0x' + bytecode,
+          from: from
+        }
+
+        return new Tx(_rawTx)
+      })
+    
+      .catch(console.log)
+  }
+
+  /**
+   * getGasPrice
+   */
+  getGasPrice() {
+    return new Promise((resolve, reject) => {
+      this.web3.eth.getGasPrice((err, gasPrice) => {
+        if (err) {
+          reject(error)
+        } else {
+          resolve(gasPrice)
+        }
+      })
+    })
+  }
+
+  /**
+   * getTransactionCount
+   */
+  getTransactionCount(address) {
+    return new Promise((resolve, reject) => {
+      this.web3.eth.getTransactionCount(address, (err, txCount) => {
+          if (err) {
+            console.log(err)
+            reject(err)
+          } else {
+            resolve(txCount)
+          }
+      })
+    })
+  }
+
+  /**
+   * sendRawTransaction
+   */
+  sendRawTransaction(tx) {
+    return new Promise((resolve, reject) => {
+      this.web3.eth.sendRawTransaction(tx, (err, txHash) => {
+          if (err) {
+            console.log(err)
+            reject(err)
+          } else {
+            resolve(txHash)
+          }
+      })
+    })
   }
 
   /**
